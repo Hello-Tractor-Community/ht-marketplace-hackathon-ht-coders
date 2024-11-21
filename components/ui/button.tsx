588f2a8da00bcd2b19011e8a5184270974a2c34f
BeatriceWambuiMbugua <beatricewambuimbugua@gmail.com>
2024-11-21 15:58:55 +0300

    Add the favicon and delete the unnecessary fonts within the application

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -5,7 +5,11 @@ import { cva, type VariantProps } from "class-variance-authority"
 import { cn } from "@/lib/utils"
 
 const buttonVariants = cva(
+<<<<<<< HEAD
   "inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0",
+=======
+  "inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0 transition duration-500",
+>>>>>>> 084f6d0 (feat: Add UI components: Button, Checkbox, Input, and Label)
   {
     variants: {
       variant: {
@@ -13,7 +17,11 @@ const buttonVariants = cva(
         destructive:
           "bg-destructive text-destructive-foreground hover:bg-destructive/90",
         outline:
+<<<<<<< HEAD
           "border border-input bg-background hover:bg-accent hover:text-accent-foreground",
+=======
+          "border border-input bg-background hover:bg-primary hover:text-accent-foreground",
+>>>>>>> 084f6d0 (feat: Add UI components: Button, Checkbox, Input, and Label)
         secondary:
           "bg-secondary text-secondary-foreground hover:bg-secondary/80",
         ghost: "hover:bg-accent hover:text-accent-foreground",
@@ -22,7 +30,11 @@ const buttonVariants = cva(
       size: {
         default: "h-10 px-4 py-2",
         sm: "h-9 rounded-md px-3",
+<<<<<<< HEAD
         lg: "h-11 rounded-md px-8",
+=======
+        lg: "h-14 rounded-md px-8",
+>>>>>>> 084f6d0 (feat: Add UI components: Button, Checkbox, Input, and Label)
         icon: "h-10 w-10",
       },
     },
@@ -35,7 +47,11 @@ const buttonVariants = cva(
 
 export interface ButtonProps
   extends React.ButtonHTMLAttributes<HTMLButtonElement>,
+<<<<<<< HEAD
     VariantProps<typeof buttonVariants> {
+=======
+  VariantProps<typeof buttonVariants> {
+>>>>>>> 084f6d0 (feat: Add UI components: Button, Checkbox, Input, and Label)
   asChild?: boolean
 }
 
